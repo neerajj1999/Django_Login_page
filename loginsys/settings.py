561dda0720b2e5fed75d6cbfa37f974f9abe7b52
Neeraj Dwivedi <60090098+neerajj1999@users.noreply.github.com>
2021-08-02 11:55:20 +0530

    Update settings.py

--- loginsys/settings.py
+++ loginsys/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b3m9y*_wlqzl7e$^j$-8q)b6b7ly1jr5&5o#p7$2vn2*9796-u'
+SECRET_KEY = 'django-insecure-b3m9y*_wlqzl7e$^j$-8q)b6b7ly1jr5&5o#p7$2vn2*9794542136-u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
